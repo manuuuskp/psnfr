45569881c768707ee16c808af1e3dfe374d6bb27
manojprabhuskp <manojprabhuskp@gmail.com>
2023-02-20 13:09:25 +0530

    #active page fix

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useNavigate} from "react-router";
+import { NavLink } from "react-router-dom";
 
 import logo from "./../../static/images/logo_2x.png";
 import CartIcon from "./../utils/icons/CartIcons";
@@ -18,6 +19,10 @@ const Header = () => {
         navigate("signup");
     }
 
+    const activeStyle = {
+        borderBottom: "2px solid red"
+    }
+
     return <header>
         <div className="header__container" tabIndex="0">
             <div>
@@ -26,8 +31,10 @@ const Header = () => {
             <div>
                 <nav>
                     <ul className="header__navlist">
-                        <li className="header__navlist--item" tabIndex="0"><a onClick={() => navigate("home")}>Home</a></li>
-                        <li className="header__navlist--item" tabIndex="0"><a onClick={() => navigate("products")}>Products</a></li>
+                        <li className="header__navlist--item" tabIndex="0">
+                            <NavLink to="home" style={({ isActive }) => isActive ? activeStyle : undefined }>Home</NavLink></li>
+                        <li className="header__navlist--item" tabIndex="0">
+                            <NavLink to="products" style={({ isActive }) => isActive ? activeStyle : undefined }>Products</NavLink></li>
                     </ul>
                 </nav>
             </div>
