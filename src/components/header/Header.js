a8df2ba7c70e4294b0a2780c6ef7136331219988
manojprabhuskp <manojprabhuskp@gmail.com>
2023-02-22 12:47:13 +0530

    #cart item fix

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import {createPortal} from "react-dom";
 import {useNavigate} from "react-router";
 import { NavLink } from "react-router-dom";
 import Cart from "../cart/Cart";
+import { cartItemContext } from "../cartreducer/CartReducer";
 import Modal from "../utils/modal/Modal";
 
 import logo from "./../../static/images/logo_2x.png";
@@ -14,6 +15,7 @@ import "./Header.scss"
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
+    const cartItemCtx = useContext(cartItemContext);
 
     const onClickSignIn = () => {
         navigate("signin");
@@ -47,7 +49,7 @@ const Header = () => {
                     <a tabIndex="0" onClick={onClickSignIn}>Sign In</a>
                     <a tabIndex="0" onClick={onClickRegister}>Register</a>
                 </div>
-                <div className="header__cartdata"><button tabIndex="0" onClick={() => setIsOpen(true)}><CartIcon /><span>0 items</span></button></div>
+                <div className="header__cartdata"><button tabIndex="0" onClick={() => setIsOpen(true)}><CartIcon /><span>{Object.entries(cartItemCtx.cartItem.cartItems).length} items</span></button></div>
             </div>
             {isOpen && createPortal(<Modal><Cart setOpen={setIsOpen}></Cart></Modal>, document.getElementById("modal-root"))}
         </div>
