935ae9501a73d5dafe2807cb1aa01cf1ffaa602f
manojprabhuskp <manojprabhuskp@gmail.com>
2023-02-23 12:17:08 +0530

    #empty cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -18,18 +18,44 @@ const Cart = (props) => {
             {
                 Object.entries(cartItemCtx.cartItem.cartItems).map(cartItm => <CartItem key={cartItm[0]} {...cartItm[1]}></CartItem>)
             }
+            {
+                Object.entries(cartItemCtx.cartItem.cartItems).length === 0 && (
+                    <div className="cart__content--noItems">
+                        <h3>No Items in your cart</h3>
+                        <span>Your Favorite items are just a click away</span>
+                    </div>
+                )
+            }
         </div>
         <div className="cart__footer">
-            <div className="cart__promo--cont">
-                <span>Promo code can be applied on the Checkout Page</span>
-            </div>
-            <div className="cart__checkout">
-                    <span>Proceed to Checkout</span>
-                <div>
-                    <span>100 Rs</span>
-                    <span>{'>'}</span>
+            {
+                Object.entries(cartItemCtx.cartItem.cartItems).length > 0 && 
+                (
+                <>
+                <div className="cart__promo--cont">
+                    <span>Promo code can be applied on the Checkout Page</span>
+                 </div>
+                <div className="cart__checkout" role="button">
+                        <span>Proceed to Checkout</span>
+                    <div>
+                        <span>100 Rs</span>
+                        <span>{'>'}</span>
+                    </div>
                 </div>
-            </div>
+                </>
+                )
+            }
+            {
+                Object.entries(cartItemCtx.cartItem.cartItems).length === 0 &&
+                (
+                    <div className="cart__checkout--cont">
+                        <button className="cart__checkout--btn" onClick={() => props.setOpen(false)}>
+                            Start Shopping
+                        </button>
+                    </div>
+                )
+            }
+            
         </div>
     </div>
     )
